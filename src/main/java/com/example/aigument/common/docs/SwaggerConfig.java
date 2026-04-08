cdb1e513ba8e6f981cb26dd32712cc4c2e910f59
aram0117 <dkfka568355@gmail.com>
2026-04-08 20:50:38 +0900

    ✨ feat: Swagger 문서 기능 추가

---
+++ src/main/java/com/example/aigument/common/docs/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.example.aigument.common.docs;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        return new OpenAPI()
+                .components(components)
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .addServersItem(new Server().url("http://localhost").description("Docker Server"));
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Aigument API Document")
+                .description("Spring Boot 3.4.0 기반 프로젝트 API 명세서")
+                .version("1.0.0");
+    }
+}
